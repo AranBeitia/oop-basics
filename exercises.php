96117109465909f5d68e46128a9e742298fe716f
AranBeitia <arantza.beitia@gmail.com>
2021-11-24 18:17:06 +0100

    base class

--- exercises.php
+++ exercises.php
@@ -1,23 +1,56 @@
 <?php
 
 class Vehicle {
+	public $name;
 	public $motor;
 	public $brand;
 	public $color;
+
+	public function __construct($name, $motor, $brand, $color) {
+		$this->name = $name;
+		$this->motor = $motor;
+		$this->brand = $brand;
+		$this->color = $color;
+	}
+
+	function __destruct() {
+		echo "The brand of " . $this->name . " is " . $this->brand;
+	}
+
+	public function startEngine() {
+		echo $this->name . ' has started ';
+	}
+
+	public function accelerate () {
+		echo $this->name . ' has accelerated ';
+	}
+
+	public function brake () {
+		echo $this->name . ' has braked ';
+	}
 }
 
-$car = new Vehicle();
-$motorbike = new Vehicle();
+$car = new Vehicle("M135 drive", "1600cc", "BMW", "black");
+$motorbike = new Vehicle('CBR', '600cc', 'Honda', 'red');
 
-$car-> motor = "1600cc";
-$car-> brand = "BMW";
-$car-> color = "black";
+// $car-> name = "M135 drive";
+// $car-> motor = "1600cc";
+// $car-> brand = "BMW";
+// $car-> color = "black";
 
-$motorbike-> motor = "600cc";
-$motorbike-> brand = "Honda";
-$motorbike-> color = "red";
+// $motorbike = [
+// 	'name' => 'CBR',
+// 	'motor' => '600cc',
+// 	'brand' => 'Honda',
+// 	'color' => 'red'
+// ];
 
 
 var_dump($car);
 var_dump($motorbike);
+
+echo $car->startEngine();
+echo $car->accelerate();
+echo $car->brake();
+//echo $motorbike->accelerate();
 
